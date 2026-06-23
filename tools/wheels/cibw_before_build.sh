86ebdfb4909006aa251fb38aa685a48998fc6939
Malcolm Smith <smith@chaquo.com>
2026-06-24 00:18:11 +0100

    Update to OpenBLAS 0.3.33

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -50,25 +50,12 @@ if [[ "$INSTALL_OPENBLAS" = "true" ]] ; then
     mkdir -p $PKG_CONFIG_PATH
 
     if [[ $OPENBLAS == "android" ]]; then
-        openblas_version=0.2.20
         pip install \
             --target $PKG_CONFIG_PATH \
-            --extra-index-url https://chaquo.com/pypi-13.1/ \
-            chaquopy-openblas==$openblas_version
+            --index-url https://chaquo.com/pypi-upstream/ \
+            chaquopy-openblas==0.3.33
         mv $PKG_CONFIG_PATH/chaquopy/* $PKG_CONFIG_PATH
         rmdir $PKG_CONFIG_PATH/chaquopy
-
-        # There is a pkgconfig file, but it's not usable because it doesn't use the
-        # `prefix` variable, so overwrite it.
-        cat <<EOF > $PKG_CONFIG_PATH/lib/pkgconfig/openblas.pc
-prefix=/usr
-Name: openblas
-Description: OpenBLAS
-Version: ${openblas_version}
-Cflags: -I\${prefix}/include
-Libs: -L\${prefix}/lib -lopenblas
-EOF
-
     else
         python -m pip install -r requirements/ci_requirements.txt
         python -c "import scipy_${OPENBLAS}; print(scipy_${OPENBLAS}.get_pkg_config())" > $PKG_CONFIG_PATH/scipy-openblas.pc
